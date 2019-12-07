95defebe19514c4c445008cb48f6662eca8997bb
boffman <stefan.berglund@gmail.com>
2019-12-07 11:43:39 +0100

    Day5 part 1

--- 2019/day05/day05_part1.py
+++ 2019/day05/day05_part1.py
@@ -1,35 +1,59 @@
 import sys
 
-def handle_opcode_1(ix, opcodes):
-    num1_pos = opcodes[ix+1]
-    num2_pos = opcodes[ix+2]
+def handle_opcode_1(ix, opcodes, parameter_modes):
+    param1 = opcodes[ix+1]
+    param2 = opcodes[ix+2]
     dst_pos = opcodes[ix+3]
-    opcodes[dst_pos] = opcodes[num1_pos] + opcodes[num2_pos]
+    if parameter_modes[0] == 0:
+        param1 = opcodes[param1]
+    if parameter_modes[1] == 0:
+        param2 = opcodes[param2]
+    value = param1 + param2
+    opcodes[dst_pos] = value
     return ix + 4
 
-def handle_opcode_2(ix, opcodes):
-    num1_pos = opcodes[ix+1]
-    num2_pos = opcodes[ix+2]
+def handle_opcode_2(ix, opcodes, parameter_modes):
+    param1 = opcodes[ix+1]
+    param2 = opcodes[ix+2]
     dst_pos = opcodes[ix+3]
-    opcodes[dst_pos] = opcodes[num1_pos] * opcodes[num2_pos]
+    if parameter_modes[0] == 0:
+        param1 = opcodes[param1]
+    if parameter_modes[1] == 0:
+        param2 = opcodes[param2]
+    value = param1 * param2
+    opcodes[dst_pos] = value
     return ix + 4
 
 def handle_opcode_3(ix, opcodes, input_value):
     dst_pos = opcodes[ix+1]
     opcodes[dst_pos] = input_value
-    return ix + 1
+    return ix + 2
 
-def handle_opcode_4(ix, opcodes):
-    output_value = opcodes[ix+1]
-    return (ix + 1, output_value)
+def handle_opcode_4(ix, opcodes, parameter_modes):
+    param1 = opcodes[ix+1]
+    if parameter_modes[0] == 0:
+        pos = param1
+    else:
+        pos = ix+1
+    value = opcodes[pos]
+    return (ix + 2, value)
 
-def set_noun_and_verb(opcodes, noun, verb):
-    opcodes[1] = noun
-    opcodes[2] = verb
+def get_opcode_and_parameter_modes(value):
+    if value == 99:
+        return [value, 0, 0, 0]
+    opcode = value % 10
+    parm3 = value // 10000
+    value -= parm3 * 10000
+    parm2 = value // 1000
+    value -= parm2 * 1000
+    parm1 = value // 100
+    return [opcode, parm1, parm2, parm3]
 
 def calc_output(opcodes):
     ix = 0
-    while True:
+    num_4s = 0
+    cur_opcode = None
+    while cur_opcode != 99:
         values = get_opcode_and_parameter_modes(opcodes[ix])
         cur_opcode = values[0]
         parameter_modes = values[1:]
@@ -40,51 +64,17 @@ def calc_output(opcodes):
         elif cur_opcode == 3:
             print("Enter input value: ")
             input_value = int(sys.stdin.readline().strip())
-            ix = handle_opcode_3(ix, opcodes, input_value, parameter_modes)
+            ix = handle_opcode_3(ix, opcodes, input_value)
         elif cur_opcode == 4:
             ix, output_value = handle_opcode_4(ix, opcodes, parameter_modes)
+            print("Output value: {}".format(output_value))
         elif cur_opcode == 99:
-            break
+            pass
         else:
             raise Exception("Unknown opcode at position {}: {}".format(ix, cur_opcode))
     return opcodes[0]
 
-def get_opcode_and_parameter_modes(value):
-    opcode = value % 10
-    parm3 = value // 10000
-    value -= parm3 * 10000
-    parm2 = value // 1000
-    value -= parm2 * 1000
-    parm1 = value // 100
-    return [opcode, parm1, parm2, parm3]
-
-print(get_opcode_and_parameter_modes(1002))
-sys.exit(0)
-
-start_opcodes = None
 with open("input.txt", "r") as infile:
     for l in infile:
-        start_opcodes = [int(x) for x in l.strip().split(",")]
-
-target_output = 19690720
-noun = 0
-verb = 0
-output = 0
-found_target_output = False
-while not found_target_output:
-    opcodes = start_opcodes.copy()
-    set_noun_and_verb(opcodes, noun, verb)
-    output = calc_output(opcodes)
-    if output == target_output:
-        found_target_output = True
-    else:
-        verb += 1
-        if verb > 99:
-            noun += 1
-            verb = 0
-            if noun > 99:
-                raise Exception("Unable to find target output")
-
-answer = 100 * noun + verb
-print("Answer: {} (noun: {}, verb: {})".format(answer,noun,verb))
-
+        opcodes = [int(x) for x in l.strip().split(",")]
+        calc_output(opcodes)
