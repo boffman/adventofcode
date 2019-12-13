0b40da013d6161cfeb9cb675d8fe98bbb7b64655
boffman <stefan.berglund@gmail.com>
2019-12-13 15:39:53 +0100

    Day 9 part 1

--- 2019/day09/day09_part1.py
+++ 2019/day09/day09_part1.py
@@ -1,9 +1,28 @@
 import sys
 
+class MemoryDict(dict):
+    def __init__(self, *args):
+        dict.__init__(self, args)
+
+    def __missing__(self, key):
+        return 0
+
+    def __getitem__(self, key):        
+        if key < 0:
+           raise Exception("Index out of bounds: {}".format(key))
+        return dict.__getitem__(self, key)
+
+    def __setitem__(self, key, value):
+        if key < 0:
+           raise Exception("Index out of bounds: {}".format(key))
+        dict.__setitem__(self, key, value)
+
 class Computer(object):
     def __init__(self, opcodes):
-        self.opcodes = opcodes
-        self.relative_code_base = 0
+        self.opcodes = MemoryDict()
+        for ix,val in enumerate(opcodes):
+            self.opcodes[ix] = val
+        self.relative_base_offset = 0
 
     def get_opcode_and_parameter_modes(self, value):
         if value == 99:
@@ -20,12 +39,13 @@ class Computer(object):
         param_ix = ix + param_no
         p_mode = parameter_modes[param_no-1]
         if p_mode == 0:
-            pos = opcodes[ix+param_no]
+            pos = opcodes[param_ix]
             return opcodes[pos]
         elif p_mode == 1:
-            return opcodes[ix+param_no]
+            return opcodes[param_ix]
         elif p_mode == 2:
-            pos = opcodes[self.relative_code_base+ix+param_no]
+            param = opcodes[param_ix]
+            pos = self.relative_base_offset+param
             return opcodes[pos]
 
     # addition
@@ -33,6 +53,8 @@ class Computer(object):
         param1 = self.get_param(1, ix, opcodes, parameter_modes)
         param2 = self.get_param(2, ix, opcodes, parameter_modes)
         dst_pos = opcodes[ix+3]
+        if parameter_modes[2] == 2:
+            dst_pos = self.relative_base_offset+opcodes[ix+3]
         value = param1 + param2
         opcodes[dst_pos] = value
         return ix + 4
@@ -42,13 +64,17 @@ class Computer(object):
         param1 = self.get_param(1, ix, opcodes, parameter_modes)
         param2 = self.get_param(2, ix, opcodes, parameter_modes)
         dst_pos = opcodes[ix+3]
+        if parameter_modes[2] == 2:
+            dst_pos = self.relative_base_offset+opcodes[ix+3]
         value = param1 * param2
         opcodes[dst_pos] = value
         return ix + 4
 
     # input
-    def handle_opcode_3(self, ix, opcodes, input_value):
+    def handle_opcode_3(self, ix, opcodes, input_value, parameter_modes):
         dst_pos = opcodes[ix+1]
+        if parameter_modes[0] == 2:
+            dst_pos = self.relative_base_offset+opcodes[ix+1]
         opcodes[dst_pos] = input_value
         return ix + 2
 
@@ -60,7 +86,7 @@ class Computer(object):
         elif parameter_modes[0] == 1:
             pos = ix+1
         elif parameter_modes[0] == 2:
-            pos = self.relative_code_base+ix+1
+            pos = self.relative_base_offset+param1
         value = opcodes[pos]
         return (ix + 2, value)
 
@@ -84,6 +110,10 @@ class Computer(object):
     def handle_opcode_7(self, ix, opcodes, parameter_modes):
         param1 = self.get_param(1, ix, opcodes, parameter_modes)
         param2 = self.get_param(2, ix, opcodes, parameter_modes)
+        dst_pos = opcodes[ix+3]
+        if parameter_modes[2] == 2:
+            dst_pos = self.relative_base_offset+opcodes[ix+3]
+
         if param1 < param2:
             opcodes[dst_pos] = 1
         else:
@@ -94,6 +124,10 @@ class Computer(object):
     def handle_opcode_8(self, ix, opcodes, parameter_modes):
         param1 = self.get_param(1, ix, opcodes, parameter_modes)
         param2 = self.get_param(2, ix, opcodes, parameter_modes)
+        dst_pos = opcodes[ix+3]
+        if parameter_modes[2] == 2:
+            dst_pos = self.relative_base_offset+opcodes[ix+3]
+
         if param1 == param2:
             opcodes[dst_pos] = 1
         else:
@@ -101,24 +135,24 @@ class Computer(object):
         return ix + 4
 
     # relative base offset
-    def handle_opcode_9(self, ix, opcodes):
-        param1 = opcodes[ix+1]
-        self.relative_code_base += param1
+    def handle_opcode_9(self, ix, opcodes, parameter_modes):
+        param1 = self.get_param(1, ix, opcodes, parameter_modes)
+        self.relative_base_offset += param1
         return ix + 2
 
-    def compute(input=None):
+    def compute(self, input=None):
         ix = 0
         num_4s = 0
         cur_opcode = None
         opcodes = self.opcodes
         while cur_opcode != 99:
-            values = get_opcode_and_parameter_modes(opcodes[ix])
+            values = self.get_opcode_and_parameter_modes(opcodes[ix])
             cur_opcode = values[0]
             parameter_modes = values[1:]
             if cur_opcode == 1:
-                ix = handle_opcode_1(ix, opcodes, parameter_modes)
+                ix = self.handle_opcode_1(ix, opcodes, parameter_modes)
             elif cur_opcode == 2:
-                ix = handle_opcode_2(ix, opcodes, parameter_modes)
+                ix = self.handle_opcode_2(ix, opcodes, parameter_modes)
             elif cur_opcode == 3:
                 input_value = None
                 if not input is None:
@@ -126,11 +160,10 @@ class Computer(object):
                 else:
                     print("Enter input value: ")            
                     input_value = int(sys.stdin.readline().strip())
-                ix = handle_opcode_3(ix, opcodes, input_value)
+                ix = self.handle_opcode_3(ix, opcodes, input_value, parameter_modes)
             elif cur_opcode == 4:
-                ix, output_value = handle_opcode_4(ix, opcodes, parameter_modes)
+                ix, output_value = self.handle_opcode_4(ix, opcodes, parameter_modes)
                 print("Output value: {}".format(output_value))
-                return output_value
             elif cur_opcode == 5:
                 ix = self.handle_opcode_5(ix, opcodes, parameter_modes)
             elif cur_opcode == 6:
@@ -141,8 +174,8 @@ class Computer(object):
                 ix = self.handle_opcode_8(ix, opcodes, parameter_modes)
             elif cur_opcode == 8:
                 ix = self.handle_opcode_8(ix, opcodes, parameter_modes)
-            elif cur_opcode == 8:
-                ix = self.handle_opcode_9(ix, opcodes)
+            elif cur_opcode == 9:
+                ix = self.handle_opcode_9(ix, opcodes, parameter_modes)
             elif cur_opcode == 99:
                 pass
             else:
@@ -150,7 +183,14 @@ class Computer(object):
         return None
 
 
+opcodes = []
 with open("input.txt", "r") as infile:
     for l in infile:
         opcodes = [int(x) for x in l.strip().split(",")]
-        calc_output(opcodes)
+
+#opcodes = [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]
+#opcodes = [1102,34915192,34915192,7,4,7,99,0]
+#opcodes = [104,1125899906842624,99]
+
+computer = Computer(opcodes)
+computer.compute()
